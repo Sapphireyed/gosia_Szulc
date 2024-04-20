72043d283566bead4d4df8e8c64c573b0eadeb73
Gosia Szulc <malgorzata@leanconvert.com>
2024-04-20 09:03:42 +0200

    add transition between pages aniamtion

--- test/src/home/home.jsx
+++ test/src/home/home.jsx
@@ -1,16 +1,34 @@
-import { useState } from 'react'
+import { useRef } from 'react'
 
 export function Home() {
+  const overlayRef = useRef(null);
+  const blockAboutRef = useRef(null);
+
+  const handleAboutClick = () => {
+    if (overlayRef.current) {
+      blockAboutRef.current.width = '100% !important';
+      blockAboutRef.current.right = '3% !important';
+      overlayRef.current.style.right = '0';
+      //boxRef.current.style.marginLeft = '-100%';
+
+      setTimeout(() => {
+        overlayRef.current.style.right = '-100%';
+        blockAboutRef.current.width = '5% !important';
+        blockAboutRef.current.right = '95% !important';
+      }, 700);
+    }
+  };
 
   return (
     <>
       <div className="container">
+        <div className="overlay" ref={overlayRef}></div>
         <div className="box">
           <div className='nav'>
 
             <div className="about">
-              <span className="block"></span>
-              <h1>About</h1>
+              <span className="block" ref={blockAboutRef}></span>
+              <h1 onClick={handleAboutClick}>About</h1>
             </div>
 
             <div className="experience">
