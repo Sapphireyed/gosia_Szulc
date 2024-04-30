90e96ca30db5b71a4dc952e67be908e62260efcf
Gosia Szulc <malgorzata@leanconvert.com>
2024-04-30 21:54:30 +0200

    added base to config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { babel } from '@rollup/plugin-babel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    babel({
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-react'],
+    }),
+  ],
   base: '/gosia_szulc/',
   css: {
     modules: true,
