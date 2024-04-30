7be418f01e635b1a9f7523a2daab666ac602348a
Gosia Szulc <malgorzata@leanconvert.com>
2024-04-30 22:03:25 +0200

    added base to config

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { babel } from '@rollup/plugin-babel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({
-      exclude: 'node_modules/**',
-      presets: ['@babel/preset-react'],
-    }),
-  ],
-  base: '/gosia_szulc/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    target: 'esnext',
-    sourcemap: true,
-    assetsInlineLimit: 4096,
-    assetsDir: 'assets',
-    cssCodeSplit: true,
-    manifest: true,
-    rollupOptions: {
-      input: './main.jsx',
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: 'chunks/[name].js',
-        assetFileNames: '[name].[ext]',
-        format: 'es',
-      },
-    },
-  },
-  css: {
-    modules: true,
-  },
+  plugins: [react()],
   assetsInclude: ['**/*.PNG'],
 })
