2e466f0e9eb3a3981aad920dc897413de7374c79
Gosia Szulc <malgorzata@leanconvert.com>
2024-05-17 17:16:49 +0200

    cleaning

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -14,5 +14,3 @@ export function Header({overlayRef, setCurrentPage, ref}) {
 }
 
 export default Header
-
-{/* <img src="https://s3-alpha-sig.figma.com/img/1be9/2566/6b3ec01eeaad05f535ce4022703a96a4?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=L2tAQUxy5f7Lnf~BPwdbL8Du9NKnrTRliWcBWszy5kSokMCzyh99b7FI~lyeyQmTJoKJ7gtlOqAkAXT09jyT3HfNjmoMs5-sDtTqjj9EqjbnGhqdNdkCjG42mc-WT~hGcPwagUtMhSCennqgjVoz4VdGI6~iY1BFRMven7sDULFD9PTKD7obGCih-ou4BZsbg4wMBkXkj6QiPB3YfofdLe4TnxWnuIs~gYQYL3bSpuOBPjeubFC4Ra7E4SATXM7reRS4sJmCMhYySjgGLU7tWLUecPrqMRTNVy45BSbbA6SibLn2OfmDkCNwXf4ZQVY9LBdKrthDftce3HI4Uo5Dxg__"> */}
